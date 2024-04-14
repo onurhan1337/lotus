0242e28f60315d848949d64577a70e4b0aec0d40
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-04-14 14:46:55 +0300

    refac: update data fetching to page

--- app/challenges/page.tsx
+++ app/challenges/page.tsx
@@ -1,19 +1,25 @@
 import type { Metadata } from "next";
-import Link from "next/link";
 
+import { getAllChallenges } from "@/actions/challenge";
+import { ChallengesList } from "@/components/challenges-list";
 import Container from "@/components/layout/container";
-import { Button } from "@/components/ui/button";
-import {ChallengesList} from "@/components/challenges-list";
+import { cache } from "react";
 
 export const metadata: Metadata = {
-    title: "Challenges",
-    description: "List of all challenges",
+  title: "Challenges",
+  description: "List of all challenges",
 };
 
-export default function ChallengesPage() {
-    return (
-        <Container title="Challenges">
-            <ChallengesList />
-        </Container>
-    );
+const loadChallenges = cache(async () => {
+  return await getAllChallenges();
+});
+
+export default async function ChallengesPage() {
+  const challenges = await loadChallenges();
+
+  return (
+    <Container title="Challenges">
+      <ChallengesList challenges={challenges} />
+    </Container>
+  );
 }
--- components/challenges-list.tsx
+++ components/challenges-list.tsx
@@ -1,18 +1,14 @@
-import { getAllChallenges } from "@/actions/challenge";
 import { ChallengeCard } from "@/components/challenge-card";
-import { cache } from "react";
+import { Challenge } from "@prisma/client";
 
-const loadChallenges = cache(async () => {
-  return await getAllChallenges();
-});
-
-export const ChallengesList = async () => {
-  const challenges = await loadChallenges();
-
+interface ChallengesListProps {
+  challenges: (Challenge & { participantCount: number })[];
+}
+export const ChallengesList = ({ challenges }: ChallengesListProps) => {
   return (
     <ul className={"w-full"}>
-      <li className={"grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"}>
-        {challenges.map((challenge) => (
+      <li className={"grid gap-4 grid-cols-1 md:grid-cols-1 lg:grid-cols-3"}>
+        {challenges.map((challenge, index) => (
           <ChallengeCard challenge={challenge} key={challenge.id} />
         ))}
       </li>
