51ec7c12b90db5600f922615ce20d6946f97e633
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-04-29 11:28:35 +0300

    feat: participant kpi card with max participant card

---
+++ components/participants-charts.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+export const MaxParticipantsChart = ({
+  value,
+  maxParticipants,
+}: {
+  value: number;
+  maxParticipants: number;
+}) => {
+  return (
+    <Card className="min-h-[175px] max-h-[175px]">
+      <CardHeader>
+        <CardTitle>Max Participants</CardTitle>
+        <CardDescription>
+          <span className="text-xs tracking-tighter text-muted-foreground">
+            Maximum number of participants allowed in the challenge.
+          </span>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <h1 className="text-2xl">
+          {value} / <b>{maxParticipants}</b>
+        </h1>
+      </CardContent>
+    </Card>
+  );
+};
+
+export const ParticipantsChart = ({ value }: { value: number }) => {
+  return (
+    <Card className="min-h-[175px] max-h-[175px]">
+      <CardHeader>
+        <CardTitle>Total Participants</CardTitle>
+        <CardDescription>
+          <span className="text-xs tracking-tighter text-muted-foreground">
+            Total number of participants in the challenge.
+          </span>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <h1 className="text-2xl font-bold">{value}</h1>
+      </CardContent>
+    </Card>
+  );
+};
