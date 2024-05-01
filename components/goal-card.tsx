60372e08129c33988d27767399c7fe40f73e0f50
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-05-01 15:47:29 +0300

    feat: add goal actions to goal card

--- components/goal-card.tsx
+++ components/goal-card.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { Goal } from "@prisma/client";
 import React from "react";
 import { toast } from "sonner";
+import { GoalCardActions } from "./goal-card-actions";
 import { Checkbox } from "./ui/checkbox";
 
 interface GoalCardProps {
@@ -29,30 +30,35 @@ export const GoalCard = ({ goal }: GoalCardProps) => {
   };
 
   return (
-    <div className="flex items-center justify-start space-x-4 -space-y-1 py-2">
-      <Checkbox
-        checked={goal.isCompleted}
-        onCheckedChange={() => startTransition(() => handleCompleteGoal(goal))}
-        disabled={isPending}
-      />
-      <article className={"flex flex-col"}>
-        <h2
-          className={cn(
-            goal.isCompleted ? "line-through" : "",
-            "text-sm leading-6 tracking-wide font-medium"
-          )}
-        >
-          {goal.name}
-        </h2>
-        <p
-          className={cn(
-            goal.isCompleted ? "line-through" : "",
-            "text-sm leading-6 tracking-tighter text-balance text-muted-foreground"
-          )}
-        >
-          {goal.description}
-        </p>
-      </article>
+    <div className="flex items-center justify-between py-2">
+      <div className="flex-1 flex items-center justify-start space-x-4 -space-y-1">
+        <Checkbox
+          checked={goal.isCompleted}
+          onCheckedChange={() =>
+            startTransition(() => handleCompleteGoal(goal))
+          }
+          disabled={isPending}
+        />
+        <article className={"flex flex-col"}>
+          <h2
+            className={cn(
+              goal.isCompleted ? "line-through" : "",
+              "text-sm leading-6 tracking-wide font-medium"
+            )}
+          >
+            {goal.name}
+          </h2>
+          <p
+            className={cn(
+              goal.isCompleted ? "line-through" : "",
+              "text-sm leading-6 tracking-tighter text-balance text-muted-foreground"
+            )}
+          >
+            {goal.description}
+          </p>
+        </article>
+      </div>
+      <GoalCardActions goalId={goal.id} />
     </div>
   );
 };
